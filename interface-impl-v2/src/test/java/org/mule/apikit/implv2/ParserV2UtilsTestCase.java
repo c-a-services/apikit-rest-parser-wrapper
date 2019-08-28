1603b29cc191e95e2b5abdee4a4390b200fc7c54
Juan Desimoni <juandesimoni@gmail.com>
2019-08-28 14:47:05 -0300

    juano

--- interface-impl-v2/src/test/java/org/mule/apikit/implv2/ParserV2UtilsTestCase.java
+++ interface-impl-v2/src/test/java/org/mule/apikit/implv2/ParserV2UtilsTestCase.java
@@ -8,9 +8,20 @@ package org.mule.apikit.implv2;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mule.apikit.common.ApiSyncUtils.isSyncProtocol;
+
+import org.mule.apikit.ParserUtils;
+import org.mule.apikit.implv2.loader.ApiSyncResourceLoader;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.junit.After;
 import org.junit.Test;
+import org.raml.v2.api.loader.ResourceLoader;
 
 public class ParserV2UtilsTestCase {
 
@@ -39,6 +50,14 @@ public class ParserV2UtilsTestCase {
     System.setProperty(ParserV2Utils.PARSER_V2_PROPERTY, "non-boolean-value");
     assertTrue(ParserV2Utils.useParserV2("#%RAML 1.0 this is an api definition"));
     assertFalse(ParserV2Utils.useParserV2("#%RAML 0.8 this is an api definition"));
+  }
+
+  @Test
+  public void findIncludesWithApiSyncAPI() throws URISyntaxException, IOException {
+    System.setProperty(ParserV2Utils.PARSER_V2_PROPERTY, "non-boolean-value");
+    ParserV2Utils.findIncludeNodes(new URI("resource::com.juani.test:raml-test:1.0.0:raml-test-1.0.0-raml.zip"),
+                                   new ApiSyncResourceLoader("resource::com.juani.test:raml-test:1.0.0:raml-test-1.0.0-raml.zip"));
+    int asda = 0;
   }
 
   @After
